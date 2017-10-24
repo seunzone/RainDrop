28304cc255f0933a435d696b0bcc01a1c3b65e4f
seunzone <darealseun@gmail.com>
2017-10-24 20:55:11 +0100

    Refactored test and JS challenge

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-module.exports = {
-	rainDrops: (number) => {
-    if(!number){
-      return "You entered an empty value";
-    }
-  if(isNaN(number)){
-      return "The value you entered is not a number";
-  }
-
-
-  let outputString = "";
-  if(number % 3 === 0 || number % 5 === 0 || number % 7 === 0){
-      if(number % 3 === 0){
-          outputString += "Pling";
-      }
-      if(number % 5 === 0){
-          outputString += "Plang";
-      }
-      if(number % 7 === 0){
-          outputString += "Plong";
-      }
-  }else{
-      outputString = number.toString();
-  }
-  
-  return outputString;
-	}
-}
+const dropApp = (value) => {
+    
+        //empty variable intended to hold the desried output
+        let output = "";
+    
+        //check if to see if thr argument is a number
+        if (typeof (value) === "number") {
+            //if the argument is a number do the following:
+            if (value === 0) {
+                return 0;
+            }
+    
+            if (value % 3 === 0) {
+                output += "Pling";
+            }
+    
+            if (value % 5 === 0) {
+                output += "Plang";
+            }
+    
+            if (value % 7 === 0) {
+                output += "Plong";
+            }
+            //if the argument is not a number this is returned
+        } else {
+            return 'Please enter a number';
+        }
+        //if the argument was a number, check if the output variable has a value
+        //then return the output
+        if (output) {
+            return output;
+            //return the argument
+        } else {
+            return value;
+        }
+    
+    };
+    
+module.exports = dropApp;
+    
--- test/apptest.js
+++ test/apptest.js
@@ -3,60 +3,35 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const dropApp = require('../app.js');
 
-describe('Make it Rain', function() {
-    //Describe Statement
-    describe("Pling", () => {
-        it("Should return Pling", () => {
-            expect(dropApp.rainDrops(9)).to.be.equal("Pling");
+describe("Rain Drop", () => {
+    describe("Handles valid input", () => {
+        it("Should return Plong for primeFactors of 28", () => {
+            assert.equal(dropApp(28), "Plong");
         });
-        it("Should return Pling", () => {
-            expect(dropApp.rainDrops(3)).to.be.equal("Pling");
+        it("Should return Plang for primeFactors of 10", () => {
+            assert.equal(dropApp(10), "Plang");
         });
-    });
-    //Descsibe: Plang
-    describe("Plang", () => {
-        it("Should return Plang", () => {
-            expect(dropApp.rainDrops(5)).to.be.equal("Plang");
+        it("Should return PlingPlang for primeFactors of 1755", () => {
+            assert.equal(dropApp(1755), "PlingPlang");
         });
-        it("Should return Plang", () => {
-            expect(dropApp.rainDrops(20)).to.be.equal("Plang");
+        it("Should return Pling for primeFactors of 3", () => {
+            assert.equal(dropApp(3), "Pling");
         });
-    });
-    //Descsibe: Plong
-    describe("Plong", () => {
-        it("Should return Plong", () => {
-            expect(dropApp.rainDrops(7)).to.be.equal("Plong");
+        it("Should return 34 for primeFactors of 34", () => {
+            assert.equal(dropApp(34), 34);
         });
-        it('Should return Plong', () => {
-            expect(dropApp.rainDrops(49)).to.be.equal('Plong');
+        it("Should return 17 for primeFactors of 17", () => {
+            assert.equal(dropApp(17), 17);
         });
-    })
-    //Descsibe: PlingPlang
-    describe("PlingPlang", () => {
-        it("Should return PlingPlang", () => {
-            expect(dropApp.rainDrops(15)).to.be.equal('PlingPlang');
-        });
-        it("Should return PlingPlang", () => {
-            expect(dropApp.rainDrops(45)).to.be.equal('PlingPlang');
+        it("Should return 0 for primeFactors of 0", () => {
+            assert.equal(dropApp(0), 0);
         });
     });
-     //Descsibe: PlingPlong
-    describe("PlingPlong", () => {
-        it("Should return PlingPlong", () => {
-            expect(dropApp.rainDrops(21)).to.be.equal('PlingPlong');
+
+    describe("Handles invalid input", () => {
+        it("Should return Please enter a number for primeFactors of 'word'", () => {
+            assert.equal(dropApp("word"), 'Please enter a number');
         });
-        it("Should return PlingPlong", () => {
-            expect(dropApp.rainDrops(63)).to.be.equal('PlingPlong');
-        });
+
     });
-    //Descsibe: PlangPlong
-    describe("PlangPlong", () => {
-        it("Should return PlangPlong", () => {
-            expect(dropApp.rainDrops(35)).to.be.equal('PlangPlong');
-        });
-        it("Should return PlangPlong", () => {
-            expect(dropApp.rainDrops(245)).to.be.equal('PlangPlong');
-        });
-    });
-    
 });
